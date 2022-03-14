6e1e1372e0d72a338ab298727c135db7e0d6654c
Ryunosuke Hashimoto <ryubb1108@gmail.com>
2022-03-14 18:47:38 +0900

    feat:@actions/githubでPRの情報を取得する

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const github = require("@actions/github");
 const core = require("@actions/core");
 
 async function run() {
   core.info("Info: Hello world!");
   console.log("デバッグ中です");
 
-  const testInput = core.getInput("test-input");
-  core.info(testInput);
+  const myToken = core.getInput("access-token");
+  console.log("output myToken", myToken);
+  const octokit = github.getOctokit(myToken);
+  console.log(octokit);
 }
 
 run();
