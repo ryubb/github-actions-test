eeb124790fdd268e82fd68392fdd7f7be25b08f0
Ryunosuke Hashimoto <ryubb1108@gmail.com>
2022-03-14 18:47:38 +0900

    feat:@actions/githubでPRの情報を取得する

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const github = require("@actions/github");
 const core = require("@actions/core");
 
 async function run() {
   core.info("Info: Hello world!");
   console.log("デバッグ中です");
 
-  const testInput = core.getInput("test-input");
-  core.info(testInput);
+  const myToken = core.getInput("access-token");
+  const octokit = github.getOctokit(myToken);
+  const data = await octokit.rest.pulls;
+
+  const pullRequestBody = github.context.payload.pull_request.body;
+  const pattern = /^#notion\s*(https:\/\/www.notion.so\/.+)/;
+  const result = pattern.exec(pullRequestBody);
+
+  console.log("result is");
+  console.log(result);
 }
 
 run();
